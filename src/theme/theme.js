7e48cb286bcab94f0386e62d92456484c1d6e0ef
aniebodavid <aniebovictor001@gmail.com>
2022-07-19 18:33:38 -0700

    add neutral colors

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,8 +1,15 @@
 export const theme = {
   colors: {
+    // Primary
     moderateBlue: "hsl(238, 40%, 52%)",
     softRed: "hsl(358, 79%, 66%)",
     lighGrayishBlue: "hsl(239, 57%, 85%)",
     paleRed: "hsl(357, 100%, 86%)",
+    //Neutral
+    darkBlue: "hsl(212, 24%, 26%)",
+    grayishBlue: "hsl(211, 10%, 45%)",
+    lightGray: "hsl(223, 19%, 93%)",
+    veryLightGray: "hsl(228, 33%, 97%)",
+    White: "hsl(0, 0%, 100%) ",
   },
 }
